64efb0001b7c434e9b6e3cd706e8fc859d32550e
Rodrigo Lopes <rodrigolpscarvalho@gmail.com>
2014-07-22 13:04:18 -0300

    heroku

--- PAP/settings.py
+++ PAP/settings.py
@@ -88,7 +88,3 @@ import os
 PROJECT_PATH = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_PATH, 'static'),
-)
