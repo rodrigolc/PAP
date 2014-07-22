06e2c35852d88739ddc8e85c9ce343cdb9e278e2
Rodrigo Lopes <rodrigolpscarvalho@gmail.com>
2014-07-22 12:44:36 -0300

    heroku static

--- PAP/settings.py
+++ PAP/settings.py
@@ -83,8 +83,12 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.6/howto/static-files/
-
+# Static asset configuration
+import os
+PROJECT_PATH = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_PATH, 'static'),
+)
