d614db041bbc530a1c6848aa3b929a4e6d43f95c
Rodrigo Lopes <rodrigolpscarvalho@gmail.com>
2014-07-22 09:45:32 -0300

    tweaks em cadastro

--- usuarios/views.py
+++ usuarios/views.py
@@ -6,7 +6,7 @@ from usuarios.models import *
 import time
 
 
-#pagina para gerar tokens. Usada para testes. por enquanto. haha.
+# pagina para gerar tokens. Usada para testes. por enquanto. haha.
 #/usuarios/tokens/
 def tokens(request):
     if request.method == 'GET':
@@ -14,8 +14,8 @@ def tokens(request):
             request,
             "usuarios/tokens/tokens.html",
             {
-            "tokens": TokenConvite.objects.all(),
-            "TokenConvite": TokenConvite
+                "tokens": TokenConvite.objects.all(),
+                "TokenConvite": TokenConvite
             }
         )
     elif request.method == 'POST':
@@ -29,8 +29,8 @@ def tokens(request):
             request,
             "usuarios/tokens/tokens.html",
             {
-            "tokens": TokenConvite.objects.all(),
-            "TokenConvite": TokenConvite
+                "tokens": TokenConvite.objects.all(),
+                "TokenConvite": TokenConvite
             }
         )
 
@@ -40,9 +40,9 @@ def usuarios(request):
     return render(
         request,
         "usuarios/usuarios/usuarios.html", {
-        "professores": Professor.objects.all,
-        "monitores": Monitor.objects.all,
-        "alunos": Aluno.objects.all,
+            "professores": Professor.objects.all,
+            "monitores": Monitor.objects.all,
+            "alunos": Aluno.objects.all,
 
         }
     )
@@ -50,20 +50,33 @@ def usuarios(request):
 
 #/usuarios/cadastro/
 def cadastro(request):
-    #GET = pagina de cadastro
+    # GET = pagina de cadastro
     if request.method == "GET":
         if 'token' in request.GET:
+            try:
+                token = TokenConvite.objects.get(token=request.GET['token'])
+            except Exception:
+                return render(
+                    request,
+                    "usuarios/tokens/token_invalido.html"
+                )
+            else:
+                if token.usado:
+                    return render(
+                        request,
+                        "usuarios/tokens/token_invalido.html"
+                    )
             return render(
                 request,
                 "usuarios/cadastro/cadastro.html", {
-                "token": request.GET['token'],
+                    "token": request.GET['token'],
                 }
             )
 
         else:
             return render(
                 request,
-                "usuarios/cadastro/token_invalido.html"
+                "usuarios/tokens/token_invalido.html"
             )
     elif request.method == "POST":
         token = request.POST['token']
@@ -86,6 +99,8 @@ def cadastro(request):
         usuario.user = user
         usuario.token = token
         usuario.save()
+        token.usado = True
+        token.save()
         return render(
             request,
             "usuarios/cadastro/usuario_cadastrado.html",
